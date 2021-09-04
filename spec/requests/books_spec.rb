1c249ecdedc3deef0aca39cbc1a789ffae364643
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2021-09-04 20:51:45 +0200

    Test delete book

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -16,7 +16,23 @@ describe 'Books API', type: :request do
 
   describe 'POST /books' do
     it 'create a new book' do
-      post '/api/v1/books', params: { book: {title: 'Confesiones de una máscara', author: 'Yukio Mishima'}}
+      expect {
+        post '/api/v1/books', params: { book: {title: 'Confesiones de una máscara', author: 'Yukio Mishima'}}
+      }.to change{ Book.count }.from(0).to(1)
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  describe 'DELETE /books/:id' do
+
+    let!(:book) { FactoryBot.create(:book, title: '1Q84', author: 'Haruki Murakami') }
+
+    it 'deletes a book' do
+      expect{
+        delete "/api/v1/books/#{book.id}"
+      }.to change{ Book.count }.from(1).to(0)
+
+      expect(response).to have_http_status(:no_content)
     end
   end
 end
