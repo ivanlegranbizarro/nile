d4b7ecdabb7788333a20d00383dd473cee0b1b51
Iván Legrán <68439095+ivanlegranbizarro@users.noreply.github.com>
2021-09-04 10:49:25 +0200

    Factory bot setup

---
+++ spec/requests/books_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Books API', type: :request do
+  it 'returns all books' do
+
+    # FactoryBot.create(:book, title: '1Q84', author: 'Haruki Murakami')
+    # FactoryBot.create(:book, title: '1984', author: 'George Orwell')
+
+    get '/api/v1/books'
+
+    expect(response).to have_http_status(:success)
+    expect(JSON.parse(response.body).size).to eq(2)
+  end
+end
